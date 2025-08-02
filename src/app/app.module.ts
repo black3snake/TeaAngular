8b8da2513a2c5472da17e5de3a3b2817b2812363
black3snake <black3snake@mail.ru>
2025-08-03 00:48:12 +0800

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { ProductComponent } from './components/pages/product/product.component';
+import { OrderComponent } from './components/pages/order/order.component';
+import { AnswersComponent } from './components/pages/answers/answers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ProductsComponent,
+    ProductComponent,
+    OrderComponent,
+    AnswersComponent
   ],
   imports: [
     BrowserModule,
